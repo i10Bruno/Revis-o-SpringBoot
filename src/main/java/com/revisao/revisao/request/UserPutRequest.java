2d014e625d7860c27305265be209fcf6e29acd07
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-09-28 12:45:51 -0300

    feat: criando nosso mapper put/post response

---
+++ src/main/java/com/revisao/revisao/request/UserPutRequest.java
@@ -0,0 +1,9 @@
+package com.revisao.revisao.request;
+
+public class UserPutRequest {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
