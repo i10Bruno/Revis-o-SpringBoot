8987a024371dee3c6637d5b9c3c8d032484018df
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-10-01 11:27:21 -0300

    feat:teste no findByid,delete e save implementado

--- src/test/java/com/revisao/revisao/service/UserServiceTest.java
+++ src/test/java/com/revisao/revisao/service/UserServiceTest.java
@@ -9,10 +9,14 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -58,6 +62,81 @@ class UserServiceTest {
         org.assertj.core.api.Assertions.assertThat(userFound).containsAll(UserExpected);
 
     }
+
+    @Test
+    @DisplayName("findALL returns Empty list when name is not found")
+    @Order(3)
+    void findByname_ReturnsEmptyList_WhenNameIsNotFound() {
+        var name = "not-found";
+        BDDMockito.when(repository.findByName(name)).thenReturn(Collections.emptyList());
+
+        var UserFound = service.findAll(name);
+        org.assertj.core.api.Assertions.assertThat(UserFound ).isNotNull().isEmpty();
+
+    }
+
+
+    @Test
+    @DisplayName("findByid  throws ResponseStatusException when producer is not found")
+    @Order(4)
+    void findByID_ReturnsUser_WhenIDIsFound() {
+        var expected=UserList.getFirst();
+        BDDMockito.when(repository.findByid(expected.getId())).thenReturn(Optional.of(expected));
+        var userFound=service.findByid(expected.getId());
+        org.assertj.core.api.Assertions.assertThat(userFound).isEqualTo(expected);
+
+    }
+    @Test
+    @DisplayName("findByid returns  a object when ID exists")
+    @Order(5)
+    void findByid_ThrowsResponseStatusException_WhenProducerIsNotFound() {
+        var expected=UserList.getFirst().getId();
+        BDDMockito.when(repository.findByid(expected)).thenReturn(Optional.empty());
+        assertThatException()
+                .isThrownBy(() -> service.findByid(expected))
+                .isInstanceOf(ResponseStatusException.class);
+
+    }
+    @Test
+    @DisplayName("Save creates a producer")
+    @Order(6)
+    void Save_CreatesProducer_WhenSuccesful() {
+       var saveUser=userUtils.newUserToSave();
+       BDDMockito.when(repository.save(saveUser)).thenReturn(saveUser);
+
+       var saved=service.save(saveUser);
+
+        org.assertj.core.api.Assertions.assertThat(saved).isEqualTo(saveUser).hasNoNullFieldsOrProperties();
+
+    }
+    @Test
+    @DisplayName("delete remove a producer")
+    @Order(7)
+    void delete_RemoveUser_whenSuccesful() {
+
+        var expectedUser=UserList.getFirst();
+        BDDMockito.when(repository.findByid(expectedUser.getId())).thenReturn(Optional.of(expectedUser));
+        BDDMockito.doNothing().when(repository).delete(expectedUser);
+        assertThatNoException().isThrownBy(() -> service.delete(expectedUser.getId()));
+
+
+    }
+    @Test
+    @DisplayName("delete throws ResponseStatusException when user is not found")
+    @Order(8)
+    void delete_ThrowsResponseStatusException_whenProducerIsNotFound() {
+        var userToDelete = UserList.getFirst();
+        BDDMockito.when(repository.findByid(userToDelete.getId())).thenReturn(Optional.empty());
+
+
+        assertThatException()
+                .isThrownBy(() -> service.delete(userToDelete.getId()))
+                .isInstanceOf(ResponseStatusException.class);
+
+    }
+
+
+
 
 
 
