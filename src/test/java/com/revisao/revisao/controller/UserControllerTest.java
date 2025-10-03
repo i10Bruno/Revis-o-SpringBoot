194984ef65818b59de3195844c913a8bfc1c7311
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-10-03 09:57:13 -0300

    feat:JSON RESPONSE E REQUEST PARA O CRUD CRIADO

---
+++ src/test/java/com/revisao/revisao/controller/UserControllerTest.java
@@ -0,0 +1,95 @@
+package com.revisao.revisao.controller;
+
+import com.revisao.revisao.Mapper.UserMapperImpl;
+import com.revisao.revisao.commons.FileUtils;
+import com.revisao.revisao.commons.UserUtils;
+import com.revisao.revisao.domain.User;
+import com.revisao.revisao.repository.UserData;
+import com.revisao.revisao.repository.UserHardCodedRepository;
+import com.revisao.revisao.service.UserService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+@Import({UserMapperImpl.class, UserService.class, UserHardCodedRepository.class,UserData.class})
+
+
+@ComponentScan("com.revisao.revisao")
+
+
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoSpyBean
+    private UserHardCodedRepository repository;
+
+
+    @MockitoBean
+    private UserData userData;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private FileUtils fileUtils;
+
+    @Autowired
+    private UserUtils userUtils;
+
+    private List<User> UserList;
+
+    @BeforeEach
+    void init() {
+
+        UserList = userUtils.newUserList();
+    }
+    @Test
+    @DisplayName("GET v1/user return a list whith all user when arguments is null")
+    @Order(1)
+    void findAll_ReturnsAllAnimes_WhenArgumentsIsNull() throws Exception {
+
+        BDDMockito.when(userData.getUSERS()).thenReturn(UserList);
+
+
+        String response = fileUtils.readResourceFile("user/get-user-null-name-200.json");
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/user"))
+                .andDo(MockMvcResultHandlers.print()).andExpect(status().isOk()).
+                andExpect(content().json(response));;
+
+    }
+    @Test
+    @DisplayName(" GET v1/Anime?param=r9 findAll returns List with found object when name exists")
+    @Order(2)
+    void findAll_ReturnsFoundAnimesInList_WhenNameIsFound() throws Exception {
+
+
+
+    }
+
+
+}
