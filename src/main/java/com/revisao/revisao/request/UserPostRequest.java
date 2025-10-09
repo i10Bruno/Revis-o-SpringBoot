ec72650f7fb5ffd963d14632f533bcd0a8d627d1
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-10-09 10:01:53 -0300

    Aprendendo sobre @Beanvalidation

--- src/main/java/com/revisao/revisao/request/UserPostRequest.java
+++ src/main/java/com/revisao/revisao/request/UserPostRequest.java
@@ -1,13 +1,17 @@
 package com.revisao.revisao.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserPostRequest {
+    @NotBlank(message = "The field' FirstName is required")
     private String firstName;
+    @NotBlank(message = "The field' LastName is required")
     private String lastName;
+    @NotBlank(message = "E-mail is not valid")
     private String email;
 
 
--- src/main/java/com/revisao/revisao/response/UserPostResponse.java
+++ src/main/java/com/revisao/revisao/response/UserPostResponse.java
@@ -7,8 +7,8 @@ import lombok.Setter;
 @Setter
 public class UserPostResponse {
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+   // private String firstName;
+   // private String lastName;
+   // private String email;
 
 }
